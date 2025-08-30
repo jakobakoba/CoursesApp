477c08215761d29fd63a77b7e04a3ea2b6c844a0
jakobakoba <borisgabyshev96dev@gmail.com>
2025-08-30 17:24:06 +0900

    create courseRepository

---
+++ domain/src/main/java/com/bor96dev/domain/CourseRepository.kt
@@ -0,0 +1,9 @@
+package com.bor96dev.domain
+
+import kotlinx.coroutines.flow.Flow
+
+
+interface CourseRepository {
+    fun getCourses(): Flow<List<Course>>
+    suspend fun updateCourseLike(courseId: String, hasLike: Boolean)
+}
