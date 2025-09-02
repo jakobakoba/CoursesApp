1335348c5f564d331ef02c8020f24e3cbabfde82
jakobakoba <borisgabyshev96dev@gmail.com>
2025-09-03 02:52:20 +0900

    favorite fragment

--- presentation/src/main/java/com/bor96dev/presentation/courses/CoursesFragment.kt
+++ presentation/src/main/java/com/bor96dev/presentation/courses/CoursesFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,7 +19,7 @@ class CoursesFragment : Fragment() {
     private var _binding: FragmentCoursesBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel: CoursesViewModel by viewModels()
+    private val viewModel: CoursesViewModel by activityViewModels()
     private lateinit var adapter: CourseAdapter
 
 
--- presentation/src/main/java/com/bor96dev/presentation/favorite/FavoriteFragment.kt
+++ presentation/src/main/java/com/bor96dev/presentation/favorite/FavoriteFragment.kt
@@ -2,59 +2,67 @@ package com.bor96dev.presentation.favorite
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bor96dev.presentation.R
+import com.bor96dev.presentation.courses.CourseAdapter
+import com.bor96dev.presentation.courses.CoursesUiState
+import com.bor96dev.presentation.courses.CoursesViewModel
+import com.bor96dev.presentation.databinding.FragmentFavoriteBinding
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FavoriteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FavoriteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: CoursesViewModel by activityViewModels()
+    private lateinit var adapter: CourseAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        observeViewModel()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = CourseAdapter { course ->
+            viewModel.toggleFavorite(course)
+        }
+
+        binding.recyclerView2.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = this@FavoriteFragment.adapter
+            setHasFixedSize(true)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FavoriteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FavoriteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun observeViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.uiState.collect { state ->
+                if (state is CoursesUiState.Success) {
+                    val favoriteCourses = state.courses.filter { it.hasLike }
+                    adapter.submitList(favoriteCourses)
                 }
             }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
